b6bf46e605b4ec499f214a24ab1f27f1c835750c
z-o-e <hyzhang22@gmail.com>
2013-08-27 19:39:25 -0400

    Update merge_sort_Inversion_count.py

--- merge_sort_Inversion_count.py
+++ merge_sort_Inversion_count.py
@@ -1,4 +1,7 @@
-""" 'IntegerArray.txt' contains all of the 100,000 integers between 1 and 100,000 (inclusive) in some order, with no integer repeated.Task is to compute the number of inversions in the file given, where the ith row of the file indicates the ith entry of an array. Because of the large size of this array, you should implement the fast divide-and-conquer algorithm covered in the video lectures. The numeric answer for the given input file should be typed in the space below.
+""" 'IntegerArray.txt' contains all of the 100,000 integers between 1 and 100,000 (inclusive) in some order, 
+    with no integer repeated.Task is to compute the number of inversions in the file given, where the ith row of 
+    the file indicates the ith entry of an array. Because of the large size of this array, you should implement 
+    the fast divide-and-conquer algorithm. 
 """
 def merge_and_count(left,right):
     assert left==sorted(left) and right==sorted(right)
