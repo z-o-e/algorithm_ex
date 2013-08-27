d9cd26d66a4fd47d4e2c9f9b4683d75b86e5be5c
z-o-e <hyzhang22@gmail.com>
2013-08-27 19:40:04 -0400

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,4 +1,7 @@
-""" 'QuickSort.txt' contains all of the integers between 1 and 10,000 (inclusive, with no repeats) in unsorted order. The integer in the ith row of the file gives you the ith entry of an input array. Task is to compute the total number of comparisons used to sort the given input file by QuickSort. As you know, the number of comparisons depends on which elements are chosen as pivots, so explore three different pivoting rules. You should not count comparisons one-by-one. Rather, when there is a recursive call on a subarray of length m, you should simply add m−1 to your running total of comparisons. (This is because the pivot element is compared to each of the other m−1 elements in the subarray in this recursive call.)
+""" 'QuickSort.txt' contains all of the integers between 1 and 10,000 (inclusive, with no repeats) in unsorted order. 
+    The integer in the ith row of the file gives you the ith entry of an input array. 
+    Task is to compute the total number of comparisons used to sort the given input file by QuickSort. As you know, 
+    the number of comparisons depends on which elements are chosen as pivots, so explore three different pivoting rules. You should not count comparisons one-by-one. Rather, when there is a recursive call on a subarray of length m, you should simply add m−1 to your running total of comparisons. (This is because the pivot element is compared to each of the other m−1 elements in the subarray in this recursive call.)
 """
 
 from random import randint
