de2b102b2cd8554c5026d7cbd68de5c9670d0e0d
z-o-e <hyzhang22@gmail.com>
2013-08-27 19:38:17 -0400

    Update karger_min_cut.py

--- karger_min_cut.py
+++ karger_min_cut.py
@@ -1,4 +1,11 @@
-"""'kargerMinCut.txt' contains the adjacency list representation of a simple undirected graph. There are 200 vertices labeled 1 to 200. The first column in the file represents the vertex label, and the particular row (other entries except the first column) tells all the vertices that the vertex is adjacent to. So for example, the 6th row looks like : "6	155	56	52	120	......". This just means that the vertex with label 6 is adjacent to (i.e., shares an edge with) the vertices with labels 155,56,52,120,......,etc. Task is to code up and run the randomized contraction algorithm for the min cut problem and use it on the above graph to compute the min cut. 
+"""'kargerMinCut.txt' contains the adjacency list representation of a simple undirected graph. 
+   There are 200 vertices labeled 1 to 200. The first column in the file represents the vertex label, 
+   and the particular row (other entries except the first column) tells all the vertices that 
+   the vertex is adjacent to. So for example, the 6th row looks like : "6  155  56  52  120......". 
+   This just means that the vertex with label 6 is adjacent to (i.e., shares an edge with) the vertices 
+   with labels 155,56,52,120,......,etc. 
+   Task is to code up and run the randomized contraction algorithm for the min cut problem and use it 
+   on the above graph to compute the min cut. 
 """
 
 import random
